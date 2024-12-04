d4e135f8d8486fc12d1ec5d679e8e8bc0ede5269
ColeWas <cwasm462@mtroyal.ca>
2024-12-04 16:34:16 -0700

    port change

--- F1_API.js
+++ F1_API.js
@@ -2,7 +2,7 @@
 const express = require('express'); 
 const app = express(); 
 const router = require('./scripts/router.js'); 
-let PORT = 8080;
+let PORT = process.env.PORT || 8080;
 // Use the router
 app.use(router);
 
